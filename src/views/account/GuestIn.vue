2dbae754b1b54583f563cdd0dd768dd57f62e0eb
hyeonsu1013 <71484658+hyeonsu1013@users.noreply.github.com>
2022-10-25 23:31:48 +0900

    게스트 로그인 일부 구현

--- src/views/account/GuestIn.vue
+++ src/views/account/GuestIn.vue
@@ -1,6 +1,9 @@
 <template>
    <div>
-        <h1>GuestIn</h1>
+        <div class="main">Login</div>
+        <div>
+            <span class="mt20">Click Here!!</span>
+        </div>
     </div>
 </template>
 
@@ -8,3 +11,7 @@
 import GuestIn from "@/views/account/guestIn";
 export default GuestIn;
 </script>
+
+<style>
+@import '@/assets/css/directorys/account.css';
+</style>
