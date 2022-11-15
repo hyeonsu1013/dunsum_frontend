e5a3424e5a22dc85403c9f2a6f46832fd0e884e9
hyeonsu1013 <71484658+hyeonsu1013@users.noreply.github.com>
2022-11-15 22:57:20 +0900

    Revert "1. Guest 로그인 기능 구현"
    This reverts commit f750c078dd036307a9cef462a566a815a3141671.

--- src/views/account/guestIn.js
+++ src/views/account/guestIn.js
@@ -1,5 +1,4 @@
 import acutApi from '@/api/account/account';
-import http from '@/api/http';
 
 let _storage = window.sessionStorage;
 
@@ -12,28 +11,8 @@ export default {
   watch: {},
 	methods: {
     guestLogin() {
-      acutApi.insGust().then(res => {
-        if(res.data != null){
-          let data = res.data;
-
-          let sessionData = {
-            lginId : data.lginId,
-            gustYn : data.gustYn,
-            pswdChngDate : data.pswdChngDate,
-          }
-
-          _storage.setItem(process.env.VUE_APP_TOKEN_KEY, data.userToken);
-          _storage.setItem(process.env.VUE_APP_USER_DATA, JSON.stringify(sessionData));
-          http.setToken(data.userToken);
-          http.setIp(data.clntIp);
-          
-          this.$router.push({
-            path: `/`,
-          });
-
-        } else {
-          console.log('res', res);
-        }
+      acutApi.insGust().then(data => {
+        console.log('data', data);
       })
       .catch(error=> {
         console.log('error', error);
@@ -42,6 +21,8 @@ export default {
   },
   created() {
     _storage.removeItem(process.env.VUE_APP_TOKEN_KEY);
-    _storage.removeItem(process.env.VUE_APP_USER_DATA);
+    // _storage.setItem(process.env.VUE_APP_TOKEN_KEY, JSON.stringify(data));
+    // http.setCustToken(token);
+
   },
 }
