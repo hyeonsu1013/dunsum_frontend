5be6153048392105ec8fc2a40562023462d4cbc8
hyeonsu1013 <71484658+hyeonsu1013@users.noreply.github.com>
2022-11-22 20:37:24 +0900

    1. 파일 생성을 위한 TemplateDefault 생성

---
+++ src/components/common/TemplateDefault.vue
@@ -0,0 +1,18 @@
+<template>
+  
+</template>
+
+<script>
+// import Template from "";
+// export default Template;
+export default {
+  name: 'Template',
+  data(){
+      return{
+      }
+  },
+  watch: {},
+	methods: {},
+  created() {},
+}
+</script>
